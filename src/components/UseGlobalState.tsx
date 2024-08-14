0ca9f27a2c4f66cebb9551ff2160c8586354b0b5
James Drury <57101727+jaymzdrury@users.noreply.github.com>
2024-08-14 13:40:26 -0400

    useDebounce and useGlobalState updates

--- src/components/UseGlobalState.tsx
+++ src/components/UseGlobalState.tsx
@@ -1,15 +1,12 @@
-import React from "react";
-import { createState, useGlobalState } from "../hooks/useGlobalState";
+import useGlobalState from "../hooks/useGlobalState";
 import MarginTop from "../ui/MarginTop";
 
-const Count = createState(0);
-
 function Counter(): JSX.Element {
-  const [count, countSet] = useGlobalState(Count);
+  const [count, countSet] = useGlobalState();
   return (
-    <React.Fragment>
+    <>
       Count: {count} <button onClick={() => countSet(count + 1)}>+</button>
-    </React.Fragment>
+    </>
   );
 }
 export default function UseGlobalState(): JSX.Element {
--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,15 +1,13 @@
 import React from "react";
 
-export default function useDebounce(
-  value: string,
-  ms: number
-): { debouncedValue: string } {
-  const [debouncedValue, debouncedValueSet] = React.useState(value);
+export default function useDebounce<T>(value: T, ms = 500) {
+  const [debouncedValue, debouncedValueSet] = React.useState<T>(value);
 
   React.useEffect(() => {
-    const id = setTimeout(() => debouncedValueSet(value), ms);
-    return () => clearTimeout(id);
+    const timeout = setTimeout(() => debouncedValueSet(value), ms);
+
+    return () => clearTimeout(timeout);
   }, [value, ms]);
 
-  return { debouncedValue };
+  return debouncedValue;
 }
